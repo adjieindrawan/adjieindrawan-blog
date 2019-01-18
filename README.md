4daf7fbaccfe77c10a8ec108e00f5cd2812d9c6b
Adjie Indrawan <adjieindrawan@gmail.com>
2019-01-18 15:22:54 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Adjie Indrawan Blog
+
+Adjie Indrawan Blog, Just for Share and Fun.
+
+## About Me
+
+I am Adjie Indrawan, UI/UX Designer - Engineer, living in Tangerang, Indonesia. I am experts in web & app design, web & app development and are able to build websites using highly technology. Currently working at PT. BFI Finance Indonesia, Tbk. You can find [my resume](https://adjieindrawan.com/).
---
+++ gatsby-config.js
@@ -0,0 +1,56 @@
+const config = require('./config')
+
+const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix
+
+module.exports = {
+  pathPrefix: config.pathPrefix,
+  siteMetadata: {
+    siteUrl: config.siteUrl + pathPrefix,
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'post',
+        path: `${__dirname}/blog`,
+      },
+    },
+    {
+      resolve: 'gatsby-mdx',
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: 'gatsby-remark-external-links',
+            options: {
+              target: '_blank',
+              rel: 'nofollow noopener noreferrer',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-autolink-headers',
+          },
+        ],
+      },
+    },
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-lodash',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: config.siteTitleAlt,
+        short_name: config.siteTitleManifest,
+        description: config.siteDescription,
+        start_url: config.pathPrefix,
+        background_color: config.backgroundColor,
+        theme_color: config.themeColor,
+        display: 'standalone',
+        icon: config.favicon,
+      },
+    },
+    'gatsby-plugin-offline',
+    'gatsby-plugin-netlify',
+  ],
+}
